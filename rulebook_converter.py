6359ed13a824f8f6a05f8693e772721a63ce422c
Rob Womble <RobWomble@users.noreply.github.com>
2021-08-15 18:56:23 -0700

    second draft of rulebook converter

--- rulebook_converter.py
+++ rulebook_converter.py
@@ -4,6 +4,14 @@
     file into a python-readable format              """
 
 # import statements here
+import json
+
+
+# define a constant with section names as a list.
+# will save lines in handbook_adapt()
+SECTIONS = ['title', 'date', 'intro',
+            'contents', 'rules',
+            'glossary', 'credits']
 
 
 def rule_dict():
@@ -15,49 +23,36 @@ def rule_dict():
         I want to split the line at the first period and make the
         rule number a key with the value of a dictionary where every
         subrule number is a key and subrule text is the value.
-        That sentence may be hard to parse, refer to the test function
+        That sentence may be hard to read, refer to the test function
         to see what I mean. Note that a period separates the rule and
         subrule, and the first subrule has a period instead of a letter.
-        Also, the text just has to end with a period, being a
-        sentence and all. I'll have to do some research to see if
-        using a .split() method will even work for me.  Maybe I'll have
-        to define another incrementable value to count how many periods
-        are in the current working line...
+        Also, the text ends with a period, since it's a sentence. I
+        still have to determine a method to break this up properly.
     '''
 
 
-def handbook_adapt(rule_input_file):
+def handbook_adapt(rule_input_file, rule_output_file):
     """ Main Function:
         defines data format,
         fills it with data,
         saves to a file     """
 
     # define the format of the final file
-    # # at this time, I'm thinking something along the lines of:
-    # rule_data = {{}, {}}
-    # #             |  | |
-    # #             |  | Overarching dictionary: for items outside of
-    # #             |  | rules and glossary that I may or may not want
-    # #             |  | to access in the other script, one key for
-    # #             |  | each section of the document
-    # #             |  |
-    # #             |  Glossary dictionary: The glossary format is
-    # #             |  "(term)\n(definition)" with empty lines above and
-    # #             |  below, so the plan for now is to use an
-    # #             |  increment in the glossary section to properly
-    # #             |  assign keys and values when there's two lines
-    # #             |  of text in a row
-    # #             |
-    # #             Rule dictionary: see rule_dict()
+    rulebook_data = {
+            SECTIONS[0]: '',  # title
+            SECTIONS[1]: '',  # date
+            SECTIONS[2]: '',  # intro, use '\n'.join()
+            SECTIONS[4]: {},  # rules (skip contents)
+            SECTIONS[5]: {},  # glossary
+            SECTIONS[6]: '',  # credits, use '\n'.join()
+    }
 
-    # variables to increment
+    # variables to increment (should be inside with?)
     # section = 0
-    # line_not_empty = 0
-    # glossary_key = ""
+    # gloss_lines = 0
+    # gloss_key = ""
 
     # fill the dataset with information from rules text
-    # I'm struggling with the idea of breaking this into separate
-    # functions without ruining the variables
     with open(rule_input_file, "r") as rulebook:
         # I still have to research the object type and applicable
         # methods for rulebook to see if the for loop will work
@@ -68,7 +63,7 @@ def handbook_adapt(rule_input_file):
             # new section: add 1 to section
 
             # elif title, date, or intro sections:
-            # add to appropriate key of rule_data
+            # join to appropriate key of rulebook_data
 
             # elif contents section: ignore
             # contents section can be reproduced by printing keys in
@@ -77,26 +72,31 @@ def handbook_adapt(rule_input_file):
             # elif rules section: call rule_dict()
 
             # elif glossary section:
+            ''' I was considering making seperate function(s) for this
+                step, but I ended up with either multiple one-line
+                functions that were called by this if statement
+                anyway, or a single function that returned different
+                types of output depending on the input, which I don't
+                know how to handle without an if statement anyway.  '''
             # the following logic should save a key if the previous
             # line was empty, add the line to the value of that key if
             # it wasn't, restart the cycle if the current line is empty
-
-                # if line_not_empty == 0:
-                    # glossary_key = line
-                    # line_not_empty += 1
-
-                # elif line == "\n":
-                    # line_not_empty = 0
-
-                # else:
-                    # add line to values of glossary_key
+            #   if line == "\n":
+            #       gloss_lines = 0
+            #   elif gloss_lines == 0:
+            #       gloss_key = line
+            #       rulebook_data['glossary'][gloss_key] = []
+            #   else:
+            #       append line to the value of
+            #       rulebook_data['glossary'][gloss_key]
 
             # else:
-                # must be credits, add to rule_data['credits']
+            #   must be credits, join to rulebook_data['credits']
 
     # create file to put rule_data into
     with open("rules.json", "r") as rule_output_file:
-        json.dump(rule_data, rule_output_file)
+        json.dump(rulebook_data, rule_output_file)
 
-if __name__ == __main__:
+
+if __name__ == "__main__":
     handbook_adapt("MagicCompRules-20210712.txt")
